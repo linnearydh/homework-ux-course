7bde093524794f41a9730dc54f5511057a727f21
linnearydh <56342922+linnearydh@users.noreply.github.com>
2019-11-27 08:49:45 +0100

    Updates

---
+++ precache-manifest.80ae9af0e46d3cab98292cb3a564eab2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b336ce53887fb9727ab1e105ac36f874",
+    "url": "/homework-lecture-3/index.html"
+  },
+  {
+    "revision": "6acb7a125cb975c37d24",
+    "url": "/homework-lecture-3/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ea39b18fcda7dc1d24aa",
+    "url": "/homework-lecture-3/static/js/2.3333ec15.chunk.js"
+  },
+  {
+    "revision": "6acb7a125cb975c37d24",
+    "url": "/homework-lecture-3/static/js/main.e3e66c3b.chunk.js"
+  },
+  {
+    "revision": "f7fe2de176c712df69b9",
+    "url": "/homework-lecture-3/static/js/runtime-main.b052e8de.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/homework-lecture-3/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.415473521e06cdb9a86c9158fa9fc6f9.js"
+  "/homework-lecture-3/precache-manifest.80ae9af0e46d3cab98292cb3a564eab2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homework-lecture-3/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
